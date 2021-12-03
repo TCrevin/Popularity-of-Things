0a965e9509e0511aca374a95b21cbcd7501b215e
MariusDia <marius.diamant@orange.fr>
2021-12-03 23:34:51 +0200

    twitter restrictions corrected
    The program now pauses if twitter has checked more than 275 min for 15min

--- Hit_Count_Engine/twitter/twitter.py
+++ Hit_Count_Engine/twitter/twitter.py
@@ -1,15 +1,7 @@
 import requests
+import time
 import json
 
-def twitter_process(a_list, eliminating):
-	res_dict = {}
-	for query in a_list:
-		fetch = Fetch(query)
-		new_count = fetch.getCount()
-		res_dict.update({query: new_count})
-	return res_dict
-		
-	
 
 class Fetch:
     def __init__(self, query):
@@ -19,7 +11,7 @@ class Fetch:
         self.query = query
         self.bearer_token = "F8vjwbTQLdew9DilAjVVKiO5QoRcCLhu"
         self.payload = {}
-        self.headers = { 
+        self.headers = {
             'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAAOt8UAEAAAAAEN0BvdqkKaVE9slkjxdut6MPSXk'
                              '%3DI4q1yypw35bZx0KGloF8vjwbTQLdew9DilAjVVKiO5QoRcCLhu '
         }
@@ -29,21 +21,36 @@ class Fetch:
         tweets_response = requests.request("GET", url, headers=self.headers, data=self.payload)
         tweets_response_text = json.loads(tweets_response.text)
         for tweet in tweets_response_text['data']:
-            print(tweet['text'],"\n\n\n")
+            print(tweet['text'], "\n\n\n")
 
     def getCount(self):
         url = "https://api.twitter.com/2/tweets/counts/recent?query=%23{}".format(self.query)
         count_response = requests.request("GET", url, headers=self.headers, data=self.payload)
         count_response_text = json.loads(count_response.text)
-        return	count_response_text['meta']['total_tweet_count']
-        print("How many tweets with #{} as the query: {}\n\n\n".format(
-            self.query,
-            count_response_text['meta']['total_tweet_count'])) 
-	
-	
+        return count_response_text['meta']['total_tweet_count']
+        #print("How many tweets with #{} as the query: {}\n\n\n".format(self.query, count_response_text['meta']['total_tweet_count']))
+
+
+def twitter_process(a_list, eliminating):
+    res_dict = {}
+    i = 1
+    for query in a_list:
+        if i%275==0:
+            print("The program is pausing to avoid twitter restrictions of 300 items/15min")
+            time.sleep(15*60)
+        print(query)
+        fetch = Fetch(query)
+        new_count = fetch.getCount()
+        res_dict.update({query: new_count})
+        print("Popularity of " + query + " : " + str(new_count) +"\n")
+        i+=1
+    return res_dict
+
+
 def main():
-	print("hello1")
-	twitter_process()
-	
+    print("hello1")
+    twitter_process()
+
+
 if __name__ == "__main__":
     main()
